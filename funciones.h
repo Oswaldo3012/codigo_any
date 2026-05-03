9012046ad5ec39bf65946445bb281588df22727c
Oswaldo3012 <oswaldo.rodriguez.escalante@udla.edu.ec>
2026-05-03 18:00:40 -0500

    Add funciones.h header file with function declarations

---
+++ funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#define MAX_NOMBRE 50
+#define MAX_PRODUCTOS 10
+
+void ingresarDatos(char nombres[][MAX_NOMBRE], float precios[], int n);
+float calcularTotal(float precios[], int n);
+float calcularPromedio(float precios[], int n);
+int indiceMasCaro(float precios[], int n);
+int indiceMasBarato(float precios[], int n);
+void buscarProducto(char nombres[][MAX_NOMBRE], float precios[], int n, char nombreBuscado[]);
+
+#endif
